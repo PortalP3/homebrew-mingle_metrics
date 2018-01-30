96559b0de674878157113aeaed1454851cff57f5
Alfredo Segundo <asegundo@thoughtworks.com>
2018-01-30 16:36:18 -0300

    mingle_metrics version v0.0.13

--- Formula/mingle_metrics.rb
+++ Formula/mingle_metrics.rb
@@ -1,9 +1,9 @@
 class MingleMetrics < Formula
   desc "Mingle metrics - client for mingle api"
   homepage ""
-  url "https://github.com/PortalP3/mingle_metrics/releases/download/v0.0.12/mingle_metrics_0.0.12_darwin_amd64.tar.gz"
-  version "0.0.12"
-  sha256 "21913de88e26164d88d76a12c6f533d6fc14aee34f141ccc54bfb43e1a85e942"
+  url "https://github.com/PortalP3/mingle_metrics/releases/download/v0.0.13/mingle_metrics_0.0.13_darwin_amd64.tar.gz"
+  version "0.0.13"
+  sha256 "eb43bf7513db7112356038b6de420d26cc44402355f9ba156e67a620daffa50d"
 
   def install
     bin.install "mingle_metrics"
